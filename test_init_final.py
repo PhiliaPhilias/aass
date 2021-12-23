66cdeb6280e6dd1b97fc7c3f543f34dfae0403fd
PhiliaPhilias <95165125+PhiliaPhilias@users.noreply.github.com>
2021-12-23 10:44:34 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -4050,7 +4050,7 @@ class IlsangDistributionBot(commands.AutoShardedBot):
 
 				for i in range(bossNum):
 					################ 보스 컷처리 ################ 
-					if message.content.startswith(bossData[i][0] +'kut') or message.content.startswith(convertToInitialLetters(bossData[i][0] +'kut')) or message.content.startswith(bossData[i][0] +' kut') or message.content.startswith(convertToInitialLetters(bossData[i][0] +' kut')):
+					if message.content.startswith(bossData[i][0] +'kut') or message.content.startswith(bossData[i][0] +' kut') :
 						if hello.find('  ') != -1 :
 							bossData[i][6] = hello[hello.find('  ')+2:]
 							hello = hello[:hello.find('  ')]
